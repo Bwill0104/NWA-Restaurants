c7880bc64f4f8ac45cb09212be5d8a371e495f77
Bryan Williams <bryanw@turing.csce.uark.edu>
2025-04-11 15:10:08 -0500

    I made the tables and put some test values in them

--- sql.sh
+++ sql.sh
@@ -2,64 +2,54 @@ mysql <<EOFMYSQL
 use bryanw;
 show tables;
 
-DROP TABLE IF EXISTS Restaurants; 
-DROP TABLE IF EXISTS Reviews;
 DROP TABLE IF EXISTS Menu;
+DROP TABLE IF EXISTS Reviews;
+DROP TABLE IF EXISTS Restaurants;
+
 
 
 CREATE TABLE Restaurants (
 restaurantID int,
-name varchar(25) NOT NULL,
-city varchar(25) NOT NULL,
-address varchar(25) NOT NULL
-hours varchar(10)
+name CHAR(25) NOT NULL,
+city CHAR(25) NOT NULL,
+address CHAR(40) NOT NULL,
+hours CHAR(10),
 PRIMARY KEY(restaurantID)
 );
-DESC Restaurants;
-INSERT INTO Restaurants (restaurantID, name, city, address, hours) VALUES ();
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES ();
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES ();
+INSERT INTO Restaurants (restaurantID, name, city, address, hours) VALUES (0, 'Texas Roadhouse', 'Rogers', '2922 S 26th st', '3-10');
+INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (1,'The Preacher''s Son', 'Bentonville', '201 NW A st', '5-9' );
 SELECT * FROM Restaurants;
 
 
 
 CREATE TABLE Reviews (
-reviewID int NOT NULL,
+reviewID int,
 restaurantID int NOT NULL,
 rating int NOT NULL,
 PRIMARY KEY(reviewID),
-FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) ON DELETE CASCADE
+FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) 
 );
-DESC Review;
-INSERT INTO Review (reviewID, restaurantID, rating) VALUES ();
-INSERT INTO Review (reviewID, restaurantID, rating) VALUES ();
-INSERT INTO Review (reviewID, restaurantID, rating) VALUES ();
-INSERT INTO Review (reviewID, restaurantID, rating) VALUES ();
-INSERT INTO Review (reviewID, restaurantID, rating) VALUES ();
-SELECT * FROM PROFESSOR;
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (0, 0, 9);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (1, 0, 8);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (2, 1, 10);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (3, 1, 9);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (4, 0, 7);
+SELECT * FROM Reviews;
 
 
 
 CREATE TABLE Menu (
 restaurantID int NOT NULL,
-cuisineType varchar(25) NOT NULL,
+cuisineType CHAR(25) NOT NULL,
 priceRange ENUM('low','med', 'high') NOT NULL,
-isVegetarian BIT,
-isGlutenFree BIT,
+isVegetarian BOOLEAN,
+isGlutenFree BOOLEAN,
 isVegan BIT,
-PRIMARY KEY (itemID),
+PRIMARY KEY (restaurantID),
 FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) ON DELETE CASCADE
 );
-DESC Menu;
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES ();
-SELECT * FROM Menu;
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0, 'idk', 'med', 1, 1, 1);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (1, 'idk' , 'high', 1,1,1);
+select * from Menu;
 
 EOFMYSQL
