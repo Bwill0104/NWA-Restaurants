1285ef875efff94e6a6fc08a557f383317677194
Bryan Williams <bryanw@turing.csce.uark.edu>
2025-04-12 17:30:14 -0500

    added more rows to the tables

--- sql.sh
+++ sql.sh
@@ -13,11 +13,17 @@ restaurantID int,
 name CHAR(25) NOT NULL,
 city CHAR(25) NOT NULL,
 address CHAR(40) NOT NULL,
-hours CHAR(10),
+hours CHAR(15),
 PRIMARY KEY(restaurantID)
 );
-INSERT INTO Restaurants (restaurantID, name, city, address, hours) VALUES (0, 'Texas Roadhouse', 'Rogers', '2922 S 26th st', '3-10');
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (1,'The Preacher''s Son', 'Bentonville', '201 NW A st', '5-9' );
+INSERT INTO Restaurants (restaurantID, name, city, address, hours) VALUES (0116, 'Texas Roadhouse', 'Rogers', '2922 S 26th st', '3pm-10pm');
+INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0601,'The Preacher''s Son', 'Bentonville', '201 NW A st', '5pm-9pm' );
+INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0630,'Feed and Folly', 'Fayetteville', '110 S College Ave', '11am-10pm' );
+INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0319,'The Rail', 'Rogers', '218 S 1st st', '11am-9pm' );
+INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0715, 'Homegrown', 'Springgdale', '202 E Emma Ave', '7am-2:30pm' );
+INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0809, 'Waffle House', 'Centerton', '960 E Centerton Blvd', '12am-12am' );
+
+
 SELECT * FROM Restaurants;
 
 
@@ -29,11 +35,13 @@ rating int NOT NULL,
 PRIMARY KEY(reviewID),
 FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) 
 );
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (0, 0, 9);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (1, 0, 8);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (2, 1, 10);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (3, 1, 9);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (4, 0, 7);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (0, 0116, 7);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (1, 0601, 8);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (2, 0601, 9);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (3, 0630, 9);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (4, 0319, 7);
+INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (5, 0809, 7);
+
 SELECT * FROM Reviews;
 
 
@@ -44,12 +52,16 @@ cuisineType CHAR(25) NOT NULL,
 priceRange ENUM('low','med', 'high') NOT NULL,
 isVegetarian BOOLEAN,
 isGlutenFree BOOLEAN,
-isVegan BIT,
+isVegan BOOLEAN,
 PRIMARY KEY (restaurantID),
 FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) ON DELETE CASCADE
 );
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0, 'idk', 'med', 1, 1, 1);
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (1, 'idk' , 'high', 1,1,1);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0116, 'idk', 'med', 1, 1, 1);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0601, 'idk' , 'high', 1,1,1);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0630, 'idk' , 'high', 1,1,0);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0319, 'idk' , 'high', 1,1,0);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0809, 'idk' , 'high', 1,1,0);
+
 select * from Menu;
 
 EOFMYSQL
