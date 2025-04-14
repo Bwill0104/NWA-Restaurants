3b9ff45565660d5c9ea049fea8fe21c1b74690dd
Bryan Williams <bryanw@turing.csce.uark.edu>
2025-04-14 14:41:05 -0500

    fix tables

--- sql.sh
+++ sql.sh
@@ -3,7 +3,7 @@ use bryanw;
 show tables;
 
 DROP TABLE IF EXISTS Menu;
-DROP TABLE IF EXISTS Reviews;
+DROP TABLE IF EXISTS Times;
 DROP TABLE IF EXISTS Restaurants;
 
 
@@ -13,36 +13,34 @@ restaurantID int,
 name CHAR(25) NOT NULL,
 city CHAR(25) NOT NULL,
 address CHAR(40) NOT NULL,
-hours CHAR(15),
+rating int,
 PRIMARY KEY(restaurantID)
 );
-INSERT INTO Restaurants (restaurantID, name, city, address, hours) VALUES (0116, 'Texas Roadhouse', 'Rogers', '2922 S 26th st', '3pm-10pm');
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0601,'The Preacher''s Son', 'Bentonville', '201 NW A st', '5pm-9pm' );
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0630,'Feed and Folly', 'Fayetteville', '110 S College Ave', '11am-10pm' );
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0319,'The Rail', 'Rogers', '218 S 1st st', '11am-9pm' );
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0715, 'Homegrown', 'Springgdale', '202 E Emma Ave', '7am-2:30pm' );
-INSERT INTO Restaurants (restaurantID, name, city, address, hours)  VALUES (0809, 'Waffle House', 'Centerton', '960 E Centerton Blvd', '12am-12am' );
-
-
+INSERT INTO Restaurants (restaurantID, name, city, address, rating) VALUES (0116, 'Texas Roadhouse', 'Rogers', '2922 S 26th st', 4.4);
+INSERT INTO Restaurants (restaurantID, name, city, address, rating)  VALUES (0601,'The Preacher''s Son', 'Bentonville', '201 NW A st', 4.4 );
+INSERT INTO Restaurants (restaurantID, name, city, address, rating)  VALUES (0630,'Feed and Folly', 'Fayetteville', '110 S College Ave', 4.1 );
+INSERT INTO Restaurants (restaurantID, name, city, address, rating)  VALUES (0319,'The Rail', 'Rogers', '218 S 1st st', 4.5 );
+INSERT INTO Restaurants (restaurantID, name, city, address, rating)  VALUES (0715, 'Homegrown', 'Springgdale', '202 E Emma Ave', 4.7 );
+INSERT INTO Restaurants (restaurantID, name, city, address, rating)  VALUES (0809, 'Waffle House', 'Centerton', '960 E Centerton Blvd', 4.2 );
 SELECT * FROM Restaurants;
 
 
 
-CREATE TABLE Reviews (
-reviewID int,
+CREATE TABLE Times (
 restaurantID int NOT NULL,
-rating int NOT NULL,
-PRIMARY KEY(reviewID),
-FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) 
+day CHAR(15) NOT NULL,
+openBreak BOOLEAN NOT NULL,
+openLunch BOOLEAN NOT NULL,
+openDinner BOOLEAN,
+PRIMARY KEY (restaurantID),
+FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) ON DELETE CASCADE
 );
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (0, 0116, 7);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (1, 0601, 8);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (2, 0601, 9);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (3, 0630, 9);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (4, 0319, 7);
-INSERT INTO Reviews (reviewID, restaurantID, rating) VALUES (5, 0809, 7);
-
-SELECT * FROM Reviews;
+INSERT INTO Times (restaurantID,day, openBreak, openLunch, openDinner) VALUES (0116, 'M,T,W,Th,F,S,Su' , 0,1,1);
+INSERT INTO Times (restaurantID,day, openBreak, openLunch, openDinner) VALUES (0601, 'M,T,W,Th,F,S,Su' , 0,0,1);
+INSERT INTO Times (restaurantID,day, openBreak, openLunch, openDinner) VALUES (0630, 'M,T,W,Th,F,S,Su' , 0,1,1);
+INSERT INTO Times (restaurantID,day, openBreak, openLunch, openDinner) VALUES (0319, 'M,T,W,Th,F,S,Su' , 1,1,0);
+INSERT INTO Times (restaurantID,day, openBreak, openLunch, openDinner) VALUES (0809, 'M,T,W,Th,F,S,Su' , 1,1,1);
+select * from Times;
 
 
 
@@ -56,11 +54,12 @@ isVegan BOOLEAN,
 PRIMARY KEY (restaurantID),
 FOREIGN KEY (restaurantID) REFERENCES Restaurants(restaurantID) ON DELETE CASCADE
 );
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0116, 'idk', 'med', 1, 1, 1);
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0601, 'idk' , 'high', 1,1,1);
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0630, 'idk' , 'high', 1,1,0);
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0319, 'idk' , 'high', 1,1,0);
-INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0809, 'idk' , 'high', 1,1,0);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0116, 'steakhouse', 'med', 1, 1, 1);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0601, 'new american' , 'high', 1,1,1);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0630, 'gastropub' , 'med', 1,1,0);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0319, 'pizza' , 'low', 1,1,0);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0715, 'brunch' , 'med', 1,1,0);
+INSERT INTO Menu (restaurantID,cuisineType, priceRange, isVegetarian, isGlutenFree, isVegan) VALUES (0809, 'brunch' , 'low', 1,1,0);
 
 select * from Menu;
 
